6fe8138eb8c76d6c0881e21fbaf61880b7f7c6bf
paulbroek <pauliusbroek@hotmail.com>
2021-09-12 23:45:47 +0200

    first commit, video_finder.py contains methods to query on multiple search terms and sort results by Custom_Score in pd dataframe

---
+++ connect.py
@@ -0,0 +1,29 @@
+"""
+    connect.py
+
+    connect to YouTube API and list videos by popularity
+
+    based on:
+        https://towardsdatascience.com/i-created-my-own-youtube-algorithm-to-stop-me-wasting-time-afd170f4ca3a
+
+    API help:
+        https://developers.google.com/youtube/v3/quickstart/python
+
+"""
+
+from googleapiclient.discovery import build
+
+# Call the YouTube API. To create API Key, go to Gogole cloud console -> Enable APIs page, and find "YouTube Data API v3"
+api_key = "api_key" # Enter your own API key – this one won’t work
+
+youtube_api = build("youtube", "v3", developerKey=api_key)
+
+results = youtube_api.search().list(q=search_terms, part="snippet", type="video",
+                                    order="viewCount", maxResults=50).execute()
+
+request = youtube.channels().list(
+    
+)
+response = request.execute()
+
+print(response)
