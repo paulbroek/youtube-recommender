67b6bd465d19f2a899990c2cccd6d80f15aa3e04
paulbroek <pauliusbroek@hotmail.com>
2022-04-30 16:44:24 +0200

    rename imported modules to adhere to style convention

--- youtube-recommender/topic_modelling.py
+++ youtube-recommender/topic_modelling.py
@@ -1,13 +1,14 @@
 """ 
-extract tope models using spacy
+extract topic models using spacy
 
 help: https://medium.com/@soorajsubrahmannian/extracting-hidden-topics-in-a-corpus-55b2214fc17d
 """
 
 import pandas as pd
 
-from gensim import models, corpora
 import spacy
+from gensim import models as gs_models
+from gensim import corpora as gs_corpora
 
 nlp = spacy.load("en_core_web_sm")
 data = pd.read_feather("data/captions.feather")
@@ -15,13 +16,12 @@ data = pd.read_feather("data/captions.feather")
 NUM_TOPIC = 10
 
 
-""" Step-1: clean up your text and generate list of words for each document. 
-I recommend you go through an introductory tutorial on Spacy in this link. 
-The content inside the cleanup function is designed for a specific action. 
-I have provided two examples in the github repo """
-
-
 def clean_up(text):
+    """ Step-1: clean up your text and generate list of words for each document. 
+    I recommend you go through an introductory tutorial on Spacy in this link. 
+    The content inside the cleanup function is designed for a specific action. 
+    I have provided two examples in the github repo """
+
     removal = ["ADV", "PRON", "CCONJ", "PUNCT", "PART", "DET", "ADP", "SPACE"]
     text_out = []
     doc = nlp(text)
@@ -37,17 +37,18 @@ def clean_up(text):
     return text_out
 
 
-datalist = data.text.apply(lambda x: clean_up(x))
+# datalist = data.text.apply(lambda x: clean_up(x))
+datalist = data.text.map(clean_up)
 
 # Step-2: Create a vocabulary for the lda model and
 # convert our corpus into document-term matrix for Lda
 
-dictionary = corpora.Dictionary(datalist)
+dictionary = gs_corpora.Dictionary(datalist)
 doc_term_matrix = [dictionary.doc2bow(doc) for doc in datalist]
 
 # Step-3 : Define multicore lda model
 
-Lda = models.LdaMulticore
+Lda = gs_models.LdaMulticore
 lda = Lda(
     doc_term_matrix,
     num_topics=NUM_TOPIC,
