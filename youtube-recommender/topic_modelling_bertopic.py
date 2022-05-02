472eb94c85d8f6c8f45a24849c14891f79fa6c59
paulbroek <pauliusbroek@hotmail.com>
2022-05-02 17:26:13 +0200

    duplicate bertopic docs when there are too few of them

--- youtube-recommender/topic_modelling_bertopic.py
+++ youtube-recommender/topic_modelling_bertopic.py
@@ -1,10 +1,27 @@
 """
 inspired by:
-	https://www.holisticseo.digital/python-seo/topic-modeling/
+    https://www.holisticseo.digital/python-seo/topic-modeling/
+
+Run:
+    cd ~/repos/youtube-recommender/youtube-recommender
+    ipy topic_modelling_bertopic.py
 """
 
 import pandas as pd
+# from sklearn.datasets import fetch_20newsgroups
 from bertopic import BERTopic
 
 data = pd.read_feather("data/captions.feather")
+docs = data["text"].to_list()
+
+# bertopic only works with many documents, not a small number
+if len(docs) < 300:
+    dupls = 10
+    to_copy = docs.copy()
+    for i in range(dupls):
+        docs += to_copy
+
 topic_model = BERTopic()
+
+# docs = fetch_20newsgroups(subset='all',  remove=('headers', 'footers', 'quotes'))['data']
+topics, probs = topic_model.fit_transform(docs)
