534fca42fe7441c17df2eb69bb1fa69e9ac8d9c7
paulbroek <pauliusbroek@hotmail.com>
2022-05-03 17:24:19 +0200

    add data_methods.py, filter non english titles before saving to feather #2

---
+++ youtube-recommender/data_methods.py
@@ -0,0 +1,60 @@
+""" data_methods.py
+
+    methods related to working with dataframes
+"""
+
+from operator import itemgetter
+import logging
+from yapic import json
+
+import pandas as pd
+import langid
+
+logger = logging.getLogger(__name__)
+
+LANGUAGE_CL = "language_cl"
+LANGUAGE_CODE = "language_code"
+
+
+def extract_video_id(df: pd.DataFrame, urlCol="Video URL") -> pd.DataFrame:
+    """extracts YouTube's video_id from plain URL
+    example: https://www.youtube.com/watch?v=t0OX4jbFwvM --> t0OX4jbFwvM
+    """
+    assert urlCol in df.columns
+    df = df.copy()
+    df["video_id"] = df[urlCol].str.rsplit("?v=", n=1).str[1]
+
+    return df
+
+
+def classify_language(df: pd.DataFrame, column: str) -> pd.DataFrame:
+    """classify the language of a pandas column"""
+
+    assert column in df.columns, f"{column=} not in {df.columns=}"
+
+    df = df.copy()
+
+    df[LANGUAGE_CL] = df[column].map(langid.classify)
+    df[LANGUAGE_CODE] = df[LANGUAGE_CL].map(itemgetter(0))
+    df[LANGUAGE_CL] = df[LANGUAGE_CL].map(json.dumps)  # makes it serializable
+
+    return df
+
+
+def keep_language(df: pd.DataFrame, lang_code: str) -> pd.DataFrame:
+    """keep only rows of language `lang_code`"""
+
+    # `classify_language` should run before this method
+    assert LANGUAGE_CL in df.columns
+    assert LANGUAGE_CODE in df.columns
+    assert lang_code in df[LANGUAGE_CODE].to_list()
+
+    rows_before = len(df)
+    df = df[df.language_code == "en"].copy()
+    rows_after = len(df)
+
+    logger.info(
+        f"keeping {rows_after:,} rows. dismissed {rows_before - rows_after} non-{lang_code} rows"
+    )
+
+    return df
