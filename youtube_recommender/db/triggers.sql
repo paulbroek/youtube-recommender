3b4ebcf3d05532ac2ee474d47f15935f6d10d14d
paulbroek <pauliusbroek@hotmail.com>
2022-05-08 04:01:48 +0200

    fixing trigger functions not triggering #5

--- youtube_recommender/db/triggers.sql
+++ youtube_recommender/db/triggers.sql
@@ -1,13 +1,5 @@
 -- trigger functions
 
-CREATE OR REPLACE FUNCTION refresh_last_videos()
-RETURNS TRIGGER LANGUAGE plpgsql
-AS $$
-BEGIN
-    REFRESH MATERIALIZED VIEW last_videos;
-    RETURN NULL;
-END $$;
-
 CREATE OR REPLACE FUNCTION refresh_last_query_results()
 RETURNS TRIGGER LANGUAGE plpgsql
 AS $$
@@ -16,14 +8,15 @@ BEGIN
     RETURN NULL;
 END $$;
 
--- triggers
+CREATE OR REPLACE FUNCTION refresh_last_videos()
+RETURNS TRIGGER LANGUAGE plpgsql
+AS $$
+BEGIN
+    REFRESH MATERIALIZED VIEW last_videos;
+    RETURN NULL;
+END $$;
 
-DROP TRIGGER IF EXISTS refresh_last_videos ON query_result;
-CREATE TRIGGER refresh_last_videos
-    AFTER INSERT OR UPDATE OR DELETE
-    ON "query_result"
-    FOR EACH ROW
-EXECUTE PROCEDURE refresh_last_videos();
+-- triggers
 
 DROP TRIGGER IF EXISTS refresh_last_query_results ON query_result;
 CREATE TRIGGER refresh_last_query_results
@@ -31,3 +24,10 @@ CREATE TRIGGER refresh_last_query_results
     ON "query_result"
     FOR EACH ROW
 EXECUTE PROCEDURE refresh_last_query_results();
+
+DROP TRIGGER IF EXISTS refresh_last_videos ON query_result;
+CREATE TRIGGER refresh_last_videos
+    AFTER INSERT OR UPDATE OR DELETE
+    ON "query_result"
+    FOR EACH ROW
+EXECUTE PROCEDURE refresh_last_videos();
