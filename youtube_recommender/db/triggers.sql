9fc96f3e9017512c6cb734aceff42a963acc77eb
paulbroek <pauliusbroek@hotmail.com>
2022-05-08 04:46:26 +0200

    fixing trigger functions not triggering #11

--- youtube_recommender/db/triggers.sql
+++ youtube_recommender/db/triggers.sql
@@ -8,16 +8,17 @@ BEGIN
     RETURN NULL;
 END $$;
 
--- CREATE OR REPLACE FUNCTION refresh_last_videos()
--- RETURNS TRIGGER LANGUAGE plpgsql
--- AS $$
--- BEGIN
---     REFRESH MATERIALIZED VIEW last_videos;
---     RETURN NULL;
--- END $$;
+CREATE OR REPLACE FUNCTION refresh_last_videos()
+RETURNS TRIGGER LANGUAGE plpgsql
+AS $$
+BEGIN
+    REFRESH MATERIALIZED VIEW last_videos;
+    RETURN NULL;
+END $$;
 
 -- triggers
 
+-- todo: why is running AFTER last record was inserted? it lags behind by 1 
 DROP TRIGGER IF EXISTS refresh_last_query_results ON query_result;
 CREATE TRIGGER refresh_last_query_results
 BEFORE INSERT OR UPDATE OR DELETE
@@ -26,9 +27,9 @@ FOR EACH ROW
 -- FOR EACH STATEMENT
 EXECUTE PROCEDURE refresh_last_query_results();
 
--- DROP TRIGGER IF EXISTS refresh_last_videos ON query_result;
--- CREATE TRIGGER refresh_last_videos
--- AFTER INSERT OR UPDATE OR DELETE
--- ON query_result
--- FOR EACH ROW
--- EXECUTE PROCEDURE refresh_last_videos();
+DROP TRIGGER IF EXISTS refresh_last_videos ON query_result;
+CREATE TRIGGER refresh_last_videos
+AFTER INSERT OR UPDATE OR DELETE
+ON query_result
+FOR EACH ROW
+EXECUTE PROCEDURE refresh_last_videos();
