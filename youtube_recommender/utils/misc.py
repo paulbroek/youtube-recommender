a594a012d7f4d2700184e15c03dae506705106d1
paulbroek <pauliusbroek@hotmail.com>
2022-07-07 14:15:55 +0200

    move load_config to models.py

--- youtube_recommender/utils/misc.py
+++ youtube_recommender/utils/misc.py
@@ -1,14 +1,8 @@
 """misc.py, miscelannous utility methods for youtube-recommender."""
 
-import configparser
-from pathlib import Path
-
 import yaml
-from rarc_utils.misc import AttrDict
-from youtube_recommender import config as config_dir
 
 ENC = "utf8"
-
 
 def load_yaml(filepath):
     """Import YAML config file."""
@@ -19,20 +13,3 @@ def load_yaml(filepath):
             print(exc)
 
     return None
-
-
-def load_config():
-    """Load config.
-
-    ugly way of retrieving postgres cfg file
-    """
-    p = Path(config_dir.__file__)
-    cfgFile = p.with_name("postgres.cfg")
-
-    parser = configparser.ConfigParser()
-    parser.read(cfgFile)
-    assert "psql" in parser, f"'psql' not in {cfgFile=}"
-    psql = AttrDict(parser["psql"])
-    assert psql["db"] == "youtube"  # do not overwrite existing other db
-
-    return psql
