9c656b7dba5ca60f6619ef58ec74faf04d6755b7
paulbroek <pauliusbroek@hotmail.com>
2022-07-12 23:23:16 +0200

    move io methods to io_methods

---
+++ youtube_recommender/io_methods.py
@@ -0,0 +1,66 @@
+"""io.methods.py, methods related to IO."""
+
+import logging
+import os
+from pathlib import Path
+from typing import Any, Dict, List, Optional
+
+import jsonlines
+import pandas as pd
+
+logger = logging.getLogger(__name__)
+
+
+class io_methods:
+    """Methods related to IO."""
+
+    @staticmethod
+    def reset_jsonlines(path: Path) -> None:
+        """Reset jsonlines file."""
+        with open(path, "w", encoding="utf"):
+            pass
+
+    @staticmethod
+    def load_jsonlines(path: Path) -> pd.DataFrame:
+        """Load jsonlines file and parse to dataframe."""
+        items: List[Dict[str, Any]] = []
+        with jsonlines.open(path) as reader:
+            for obj in reader:
+                items.append(obj)
+
+        df = pd.DataFrame(items)
+
+        return df
+
+    @staticmethod
+    def append_jsonlines(path: Path, items: List[Dict[str, Any]]) -> None:
+        """Write intermediary results to jsonlines file."""
+        with jsonlines.open(path, mode="a") as writer:
+            for item in items:
+                writer.write(item)
+
+    @staticmethod
+    def load_feather(df_path: Path, what: Optional[str]) -> pd.DataFrame:
+        """Load any dataframe from feather."""
+        # check if file exists, or warn user to run main.py first
+        assert os.path.exists(df_path), f"{df_path.as_posix()} does not exist"
+        assert what is not None
+        df: pd.DataFrame = pd.read_feather(df_path)
+
+        WHATS = what + "s" if len(df) != 1 else what
+        logger.info(f"loaded {len(df):,} {WHATS} rows from feather")
+
+        return df
+
+    @staticmethod
+    def save_feather(
+        df: pd.DataFrame, df_path: Path, what: Optional[str] = None
+    ) -> None:
+        """Save dataframe to feather."""
+        assert isinstance(df, pd.DataFrame)
+        assert isinstance(df_path, Path)
+        assert what is not None
+
+        df.to_feather(df_path)
+        WHATS = what + "s" if len(df) != 1 else what
+        logger.info(f"saved {len(df):,} {WHATS} to {df_path.as_posix()}")
