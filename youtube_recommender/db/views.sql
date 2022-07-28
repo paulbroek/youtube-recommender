c0abc9dfebeffefcba0595343e340f1b82cec746
paulbroek <pauliusbroek@hotmail.com>
2022-07-29 00:48:14 +0200

    update views

--- youtube_recommender/db/views.sql
+++ youtube_recommender/db/views.sql
@@ -213,6 +213,9 @@ SELECT DISTINCT
     channel.id AS channel_id,
     COUNT(DISTINCT video.id) AS video_count,
     COUNT(comment.id) AS comment_count
+    -- summing here creates duplicate sums
+    -- SUM(video.views) AS total_views,
+    -- LPAD(TO_CHAR(SUM(video.views), 'fm999G999G999G999'), 15) AS total_views_fmt
 FROM
     video
     INNER JOIN channel ON video.channel_id = channel.id
