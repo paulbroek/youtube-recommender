3b9fc97330c4145c6e548f7f30adb7fb7c08ed99
paulbroek <pauliusbroek@hotmail.com>
2022-08-28 18:56:27 +0200

    balancing grpc calls through nginx reverseproxy

--- youtube_recommender/scrape_requests/serve.py
+++ youtube_recommender/scrape_requests/serve.py
@@ -13,13 +13,24 @@ from channel_scrape_requests import ChannelScrapeService
 from comment_scrape_requests import CommentScrapeService
 from video_scrape_requests import VideoScrapeService
 
-# todo: fix with docker-secrets
-# with open("/cert/nginx.crt", "rb") as f:
-#     trusted_certs = f.read()
+with open("/run/secrets/nginx.key", "rb") as f:  # path to you key location
+    private_key = f.read()
+with open("/run/secrets/nginx.cert", "rb") as f:
+    certificate_chain = f.read()
+
+server_credentials = grpc.ssl_server_credentials(
+    (
+        (
+            private_key,
+            certificate_chain,
+        ),
+    )
+)
 
 
-def serve(max_workers):
-    # todo: does this workflow needs async functionality?
+def serve(max_workers, secure=False):
+    # todo: does this workflow need async functionality?
+    # no, because mostly clients use async calls, always be carefull to use async functionality in server
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=max_workers))
     scrape_requests_pb2_grpc.add_ChannelScrapingsServicer_to_server(
         ChannelScrapeService(), server
@@ -30,12 +41,21 @@ def serve(max_workers):
     scrape_requests_pb2_grpc.add_CommentScrapingsServicer_to_server(
         CommentScrapeService(), server
     )
-    server.add_insecure_port("[::]:50051")
+    if secure:
+        server.add_secure_port("[::]:50051", server_credentials)
+    else:
+        server.add_insecure_port("[::]:50051")
+
     server.start()
     server.wait_for_termination()
 
 
 parser = argparse.ArgumentParser(description="cli parameters")
+parser.add_argument(
+    "--secure",
+    action="store_true",
+    help="secure gRPC channels with SSL",
+)
 parser.add_argument(
     "--max_workers",
     type=int,
@@ -45,4 +65,4 @@ parser.add_argument(
 
 if __name__ == "__main__":
     cli_args = parser.parse_args()
-    serve(cli_args.max_workers)
+    serve(cli_args.max_workers, secure=cli_args.secure)
