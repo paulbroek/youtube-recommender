cd331718a80f4f87588f9aa023fa22356d4d5f9b
paulbroek <pauliusbroek@hotmail.com>
2022-08-28 02:14:44 +0200

    v0.1.0 scrape Channel, Video and Comment data through scalable microservices, using RPC with protobuf

--- youtube_recommender/scrape_requests/scrape_requests_pb2_grpc.py
+++ youtube_recommender/scrape_requests/scrape_requests_pb2_grpc.py
@@ -125,3 +125,64 @@ class VideoScrapings(object):
             scrape__requests__pb2.VideoScrapeResponse.FromString,
             options, channel_credentials,
             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+
+class CommentScrapingsStub(object):
+    """Missing associated documentation comment in .proto file."""
+
+    def __init__(self, channel):
+        """Constructor.
+
+        Args:
+            channel: A grpc.Channel.
+        """
+        self.Scrape = channel.unary_unary(
+                '/CommentScrapings/Scrape',
+                request_serializer=scrape__requests__pb2.ScrapeRequest.SerializeToString,
+                response_deserializer=scrape__requests__pb2.CommentScrapeResponse.FromString,
+                )
+
+
+class CommentScrapingsServicer(object):
+    """Missing associated documentation comment in .proto file."""
+
+    def Scrape(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
+
+def add_CommentScrapingsServicer_to_server(servicer, server):
+    rpc_method_handlers = {
+            'Scrape': grpc.unary_unary_rpc_method_handler(
+                    servicer.Scrape,
+                    request_deserializer=scrape__requests__pb2.ScrapeRequest.FromString,
+                    response_serializer=scrape__requests__pb2.CommentScrapeResponse.SerializeToString,
+            ),
+    }
+    generic_handler = grpc.method_handlers_generic_handler(
+            'CommentScrapings', rpc_method_handlers)
+    server.add_generic_rpc_handlers((generic_handler,))
+
+
+ # This class is part of an EXPERIMENTAL API.
+class CommentScrapings(object):
+    """Missing associated documentation comment in .proto file."""
+
+    @staticmethod
+    def Scrape(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/CommentScrapings/Scrape',
+            scrape__requests__pb2.ScrapeRequest.SerializeToString,
+            scrape__requests__pb2.CommentScrapeResponse.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
