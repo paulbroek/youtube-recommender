f9dabd01c84485badb2faefb2bfba98f10602746
paulbroek <pauliusbroek@hotmail.com>
2022-09-06 13:34:01 +0200

    move requirements to setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,35 @@
 
 from setuptools import find_packages, setup
 
-requires = []
-# requires = [
-#     "yapic.json>=1.7.0",
-#     "pandas>=1.0.3",
-#     "timeago>=1.0.15",
-#     "sqlalchemy>=1.4.23",
-#     "PyYAML>=5.4.1",
-# ]
+# requires = []
+requires = [
+    "yapic.json>=1.7.0",
+    "pandas>=1.0.3",
+    "timeago>=1.0.15",
+    "sqlalchemy>=1.4.23",
+    "google-api-python-client>=2.58.0",
+    "youtube-transcript-api>=0.4.4",
+    "langid>=1.1.6",
+    "pyarrow",
+    "pytube",
+    "youtube_comment_downloader",
+    "cqlengine",
+    "aiocache",
+    "aioredis==1.3.1",
+    "jsonlines",
+    "types-protobuf",
+    "grpcio-tools",
+    "grpc-interceptor~=0.12.0",
+    "pytest",
+]
+
+dev_requires = [
+    "spacy",
+    "gensim",
+    "bertopic",
+    "pre-commit",
+    "spacytextblob",
+]
 
 setup(
     name="youtube_recommender",
@@ -21,6 +42,7 @@ setup(
     author_email="pcbroek@paulbroek.nl",
     license="unlicense",
     install_requires=requires,
+    extras_require={"dev": dev_requires},
     packages=find_packages(exclude=["tests"]),
     python_requires=">=3.8",
     zip_safe=False,
