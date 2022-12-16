4db5f1b7d9596256615e02cfb8040d045a76b1c5
Paul Broek <pauliusbroek@hotmail.com>
2022-12-16 23:28:21 +0100

    use update sqlalchemy_base.load_config

--- youtube_recommender/db/config.py
+++ youtube_recommender/db/config.py
@@ -5,25 +5,26 @@ from pathlib import Path
 from rarc_utils.misc import AttrDict
 from youtube_recommender import config as config_dir
 
+# TOOD: use rarc_utils.sqlalchemy_base.load_config
 
-def load_config():
-    """Load config.
+# def load_config():
+#     """Load config.
 
-    ugly way of retrieving postgres cfg file
-    """
-    releaseMode = os.environ.get("RELEASE_MODE", "DEVELOPMENT")
-    # take from secrets dur if running in production: kubernetes
-    cfgFile = "postgres.cfg"
-    cfgPath = (
-        Path(config_dir.__file__).with_name(cfgFile)
-        if releaseMode == "DEVELOPMENT"
-        else Path("/run/secrets") / cfgFile / "secret.file"
-    )
+#     ugly way of retrieving postgres cfg file
+#     """
+#     releaseMode = os.environ.get("RELEASE_MODE", "DEVELOPMENT")
+#     # take from secrets dur if running in production: kubernetes
+#     cfgFile = "postgres.cfg"
+#     cfgPath = (
+#         Path(config_dir.__file__).with_name(cfgFile)
+#         if releaseMode == "DEVELOPMENT"
+#         else Path("/run/secrets") / cfgFile / "secret.file"
+#     )
 
-    parser = configparser.ConfigParser()
-    parser.read(cfgPath)
-    assert "psql" in parser, f"'psql' not in {cfgPath=}"
-    psql = AttrDict(parser["psql"])
-    assert psql["db"] == "youtube"  # do not overwrite existing other db
+#     parser = configparser.ConfigParser()
+#     parser.read(cfgPath)
+#     assert "psql" in parser, f"'psql' not in {cfgPath=}"
+#     psql = AttrDict(parser["psql"])
+#     assert psql["db"] == "youtube"  # do not overwrite existing other db
 
-    return psql
+#     return psql
--- youtube_recommender/utils/db_conn.py
+++ youtube_recommender/utils/db_conn.py
@@ -3,6 +3,7 @@ import pandas as pd
 import psycopg2
 from config.config import config
 
+
 # Take in a PostgreSQL table and outputs a pandas dataframe
 def load_db_table(config_db, query):
     params = config(config_db)
